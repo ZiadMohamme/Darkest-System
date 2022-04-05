8a184861d07393816341d4c664f4a32b4a6702c4
ZiadMohamme <100944438+ZiadMohamme@users.noreply.github.com>
2022-04-05 20:32:52 +0200

    edit

--- dist/functions/ServerStatusUpdate.js
+++ dist/functions/ServerStatusUpdate.js
@@ -1,7 +1,7 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+  value: true,
 });
 exports.default = void 0;
 
@@ -11,16 +11,29 @@ var _static = _interopRequireDefault(require("../assets/static"));
 
 var _subFunctions = require("../assets/subFunctions");
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
 
-const ServerStatusUpdate = guild => {
-  _static.default.liveStatus.Roles.forEach(async liveUpdate => {
+const ServerStatusUpdate = (guild) => {
+  _static.default.liveStatus.Roles.forEach(async (liveUpdate) => {
     let role = guild.roles.cache.get(liveUpdate.id);
-    let channel = guild.channels.cache.get(await (0, _subFunctions.makeliveServerStatus)(guild, role ? role : {
-      name: 'notFound',
-      id: '0'
-    }));
-    await channel.setName(`●──『${liveUpdate.name.toLowerCase() === 'members' ? `👥${(0, _helpers.fontGenerator)(liveUpdate.name)}` : liveUpdate.name.toLowerCase() === 'streamer' ? `🎮${(0, _helpers.fontGenerator)(liveUpdate.name)}` : (0, _helpers.fontGenerator)(liveUpdate.name)}』${role.members.map(i => i.name).length}`);
+    let channel = guild.channels.cache.get(
+      await (0, _subFunctions.makeliveServerStatus)(
+        guild,
+        role
+          ? role
+          : {
+              name: "notFound",
+              id: "0",
+            }
+      )
+    );
+    await channel.setName(
+      `●──『${fontGenerator(liveUpdate.name)}』${
+        role.members.map((i) => i.name).length
+      }`
+    );
   });
 };
 
