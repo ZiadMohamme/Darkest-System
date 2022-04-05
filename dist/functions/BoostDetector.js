af4e8ab02c05d2aeac89d248f07b562c3a9385a6
ZiadMohamme <100944438+ZiadMohamme@users.noreply.github.com>
2022-04-06 00:11:02 +0200

    fix err

--- dist/functions/BoostDetector.js
+++ dist/functions/BoostDetector.js
@@ -1,7 +1,7 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+  value: true,
 });
 exports.default = void 0;
 
@@ -9,24 +9,44 @@ var _discord = require("discord.js");
 
 var _static = _interopRequireDefault(require("../assets/static"));
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
 
 const BoostDetector = (oldState, newState) => {
   const oldStatus = oldState?.premiumSince;
   const newStatus = newState?.premiumSince;
-  const boostChannel = oldState?.guild.channels.cache.get(_static.default.boostChannelId);
+  const boostChannel = oldState?.guild.channels.cache.get(
+    _static.default.boostChannelId
+  );
 
   if (!oldStatus && newStatus) {
-    const embed = new _discord.MessageEmbed().setColor('#ff1493').setTitle('Server Boosted').setDescription(`${newState} Boosted The Server!!!`).addField('Total Boosts', newState.guild.premiumSubscriptionCount, false);
+    const embed = new _discord.MessageEmbed()
+      .setColor("#ff1493")
+      .setTitle("Server Boosted")
+      .setDescription(`${newState} Boosted The Server!!!`)
+      .addField(
+        "Total Boosts",
+        `${newState?.guild?.premiumSubscriptionCount}`,
+        false
+      );
     boostChannel.send({
-      embeds: [embed]
+      embeds: [embed],
     });
   }
 
   if (!newStatus && oldStatus) {
-    const embed = new _discord.MessageEmbed().setColor('#32174d').setTitle('Server Unboosted').setDescription(`${newState} Unboosted The Server!!!`).addField('Total Boosts', newState.guild.premiumSubscriptionCount, false);
+    const embed = new _discord.MessageEmbed()
+      .setColor("#32174d")
+      .setTitle("Server Unboosted")
+      .setDescription(`${newState} Unboosted The Server!!!`)
+      .addField(
+        "Total Boosts",
+        `${newState?.guild?.premiumSubscriptionCount}`,
+        false
+      );
     boostChannel.send({
-      embeds: [embed]
+      embeds: [embed],
     });
   }
 };
